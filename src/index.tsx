7f7c4202438d344a80a7ed4591f241b64795069b
Serhii Kuzmychov <ku3mich@gmail.com>
2024-03-02 22:57:28 +0200

    init

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'leaflet/dist/leaflet.css';
+import './styles.scss';
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {}
+    }
+}
+
+const App = () => {
+    return <h1>Hello World</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
---
+++ webpack.config.cjs
@@ -0,0 +1,51 @@
+﻿const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+});
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    plugins: [htmlPlugin, new Dotenv()],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                },
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        port: 3000,
+        allowedHosts: 'all',
+    },
+};
